9a0da47107fbcd5087731b49a7fbe6240dbb807d
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 01:44:02 +0300

    new config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,17 @@
-import * as dotenv from 'dotenv'
+/* eslint-disable no-return-assign */
 
-dotenv.config();
+import dotenv from 'dotenv'
+import each from 'lodash/each'
+
+const result = dotenv.config()
+
+let envs
+
+if (!('error' in result)) {
+  envs = result.parsed
+} else {
+  envs = {}
+  each(process.env, (value, key) => envs[key] = value)
+}
+
+module.exports = envs
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
-import './config'
+import { PORT } from './config'
 
 const app = express()
 
 app.use(morgan('tiny'))
 app.use(router)
 
-const { PORT: port } = process.env
-app.listen(port, () => {
-  console.log(`Listening on ${port}`)
+// const { PORT: port } = process.env
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
 })
