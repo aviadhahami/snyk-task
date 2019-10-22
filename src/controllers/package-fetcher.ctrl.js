7ed40917edc03a53a5e8a1e5e02f5a3e814778c6
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 17:27:25 +0300

    small improvements

--- src/controllers/package-fetcher.ctrl.js
+++ src/controllers/package-fetcher.ctrl.js
@@ -1,33 +1,5 @@
-import axios from 'axios';
 import { fetchDepsTree } from '../logic/deps-tree-fetcher';
 import { printTree } from '../logic/tree-printer';
-import { Node } from './../structs/Node';
-import { generateURL } from '../api';
-
-const fetch = async ({ packageName, packageVersion = 'latest' }) => {
-	const url = generateURL({ name: packageName, version: packageVersion });
-	try {
-		const {
-			data: {
-				dependencies = {}
-			}
-		} = await axios.get(url);
-
-		const tree = new Node({ name: packageName, version: packageVersion });
-
-		if (Object.keys(dependencies).length) {
-			const children = await Promise.all(
-				Object.entries(dependencies)
-					.map(async ([name, version]) => fetch({ packageName: name, packageVersion: version }))
-			);
-			tree.children = children;
-		}
-		return tree;
-	} catch (e) {
-		const tree = new Node({ name: packageName, version: packageVersion });
-		return tree;
-	}
-};
 
 async function packageFetcher (req, res) {
 	const { packageName } = req.params;
--- src/logic/deps-tree-fetcher/index.js
+++ src/logic/deps-tree-fetcher/index.js
@@ -1,7 +1,29 @@
+import axios from 'axios';
+import { generateURL } from '../../api';
+import { Node } from '../../structs/Node';
 
-async function fetchDepsTree () {
+const fetchDepsTree = async ({ packageName, packageVersion = 'latest' }) => {
+	const url = generateURL({ name: packageName, version: packageVersion });
+	try {
+		const {
+			data: {
+				dependencies = {}
+			}
+		} = await axios.get(url);
 
-}
+		const tree = new Node({ name: packageName, version: packageVersion });
+
+		if (Object.keys(dependencies).length) {
+			tree.children = await Promise.all(
+				Object.entries(dependencies)
+					.map(async ([name, version]) => fetchDepsTree({ packageName: name, packageVersion: version }))
+			);
+		}
+		return tree;
+	} catch (e) {
+		return new Node({ name: packageName, version: packageVersion });
+	}
+};
 export {
 	fetchDepsTree
 };
