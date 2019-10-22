81593114950114e01fbba40fbda08581df36d1e5
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 19:35:45 +0300

    nice

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,12 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default (props) => {
+	const [ssrDone, setSsrState] = useState(false);
+	useEffect(() => {
+		setSsrState(tree);
+	});
 	const { tree = {} } = props;
-	debugger
-	return (
-		<div>
-			<h2>Hi there...{Object.keys(tree).length.toString()}</h2>
-		</div>
-	);
+
+	if (!ssrDone) {
+		return (
+			<div>
+				<h2>Loading.....</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<h2>Hi there...{Object.keys(tree).length.toString()}</h2>
+			</div>
+		);
+	}
 };
