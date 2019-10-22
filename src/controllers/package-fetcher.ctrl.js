f5356f38b62116a83aa5dd5f62328cec1716a700
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 17:39:11 +0300

    another edep

--- src/controllers/package-fetcher.ctrl.js
+++ src/controllers/package-fetcher.ctrl.js
@@ -2,9 +2,10 @@ import { fetchDepsTree } from '../logic/deps-tree-fetcher';
 import { printTree } from '../logic/tree-printer';
 
 async function packageFetcher (req, res) {
-	const { packageName } = req.params;
+	const { packageName, packageVersion } = req.params;
+	console.log(req.params);
 	try {
-		const tree = await fetchDepsTree({ packageName });
+		const tree = await fetchDepsTree({ packageName, packageVersion });
 		printTree(tree);
 		return res.status(200).send(tree);
 	} catch (e) {
