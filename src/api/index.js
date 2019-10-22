0e56b758a422b676f32a90fa4fc507cf1941d234
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 17:50:39 +0300

    another attempt

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 import findVersions from 'find-versions';
 import { API } from './../config';
 
-const generateURL = ({ name, version = 'latest' }) => {
+const LATEST = 'latest';
+const generateURL = ({ name, version = LATEST }) => {
 	const buffer = [];
 	buffer.push(API);
 	buffer.push(name);
-	if (!name.includes('@')) {
+	if (!name.includes('@') && version !== LATEST) {
 		buffer.concat(findVersions(version, { loose: true }));
 	}
 	return buffer.join('/');
