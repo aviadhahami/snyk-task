ee8acea9c525ab9f857dd4ae94510655e590e0c4
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-22 17:31:01 +0300

    route change

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,6 @@ import { packageFetcher } from '../controllers/package-fetcher.ctrl';
 const router = new Router();
 
 // regex to match namespaced packages i.e. @babel/core
-router.get('/:packageName([@\\/]\\w+\\/\\w+|\\S+)', packageFetcher);
+router.get('/packages/:packageName([@\\/]\\w+\\/\\w+|\\S+)', packageFetcher);
 
 export default router;
