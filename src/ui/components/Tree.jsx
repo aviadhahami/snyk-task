7114cc6f51946780b07f57e4a074c88604613cfe
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-24 19:33:20 +0300

    getting there

--- src/ui/components/Tree.jsx
+++ src/ui/components/Tree.jsx
@@ -3,50 +3,37 @@ import React, { useEffect, useState, useRef } from 'react';
 
 const Tree = (props) => {
 	const [windowExists, setWindowState] = useState(false);
-	const [translate, setTranslate] = useState({ x: 0, y: 0 });
-	const treeContainer = useRef(null);
-
-	const containerStyles = {
-		width: '100%',
-		height: '100vh'
-	};
 
 	useEffect(() => {
 		if (window !== undefined) {
 			setWindowState(true);
 		}
-
-		if (treeContainer.current) {
-			const dimensions = treeContainer.current.getBoundingClientRect();
-
-			setTranslate({
-				x: dimensions.width / 2,
-				y: dimensions.height / 2
-			});
-		}
 	});
+
+	const [cursor, setCursor] = useState(false);
+
+	const onToggle = (node, toggled) => {
+		if (cursor) {
+			cursor.active = false;
+		}
+		node.active = true;
+		if (node.children) {
+			node.toggled = toggled;
+		}
+		setCursor(node);
+	};
 
 	const { data } = props;
 	if (windowExists) {
-		const { Tree: D3Tree } = require('react-d3-tree');
-		return (
-			<div style={containerStyles} ref={treeContainer}>
-				<D3Tree
-					data={data}
-					translate={translate}
-					zoomable={true}
-					zoom={0.5}
-					orientation={'vertical'}
-				/>
-			</div>
-		);
-	} else {
-		return (
-			<div>
-				<p>Something is off</p>
-			</div>
-		);
+		const { Treebeard } = require(/* react-treebeard */'react-treebeard');
+		return (<Treebeard data={data} onToggle={onToggle}/>);
 	}
+
+	return (
+		<div>
+			<p>Something is off</p>
+		</div>
+	);
 };
 
 export default Tree;
