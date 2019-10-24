343a286e5efb14a24fd508f3caa625032dfa5e1d
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-24 23:30:24 +0300

    showing deps #

--- src/structs/Node.js
+++ src/structs/Node.js
@@ -6,6 +6,20 @@ class Node {
 		this.version = version;
 		this.children = children;
 	}
+	get subtreeSize () {
+		return 1 + this.children.reduce((sum, current) => sum + current.subtreeSize, 0);
+	}
+
+	toJSON () {
+		const { id, name, version, children, subtreeSize } = this;
+		return {
+			id,
+			name,
+			version,
+			children,
+			subtreeSize
+		};
+	}
 }
 
 export {
