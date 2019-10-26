a127bca1c1041704ac6065d70f4ba0cc34492e71
aviadhahami <aviadhahami@gmail.com>
2019-10-27 00:03:29 +0300

    attempting cache

--- src/logic/deps-tree-fetcher/index.js
+++ src/logic/deps-tree-fetcher/index.js
@@ -3,41 +3,38 @@ import { generateURL } from '../../api';
 import { Node } from '../../structs/Node';
 import Cache, { packageToCache } from './../../cache';
 
-const cacheOrFetch = async (name, version) => {
-	const cacheName = packageToCache(name, version);
-	const entry = await Cache.get(cacheName)
-	if(entry){
-		return entry;
-	}else{
-		const url = generateURL({ name: packageName, version: packageVersion });
-		
-		const {
-			data: {
-				dependencies = {}
-			}
-		} = await axios.get(url);
-	}
-};
-
-const fetchDepsTree = async ({ packageName, packageVersion = 'latest', firstInvoke = false }) => {
+const fetchDepsTree = async ({ packageName, packageVersion = 'latest' }) => {
 	const tree = new Node({ name: packageName, version: packageVersion });
+	const cacheName = packageToCache(packageName, packageVersion);
 	try {
-		
+		const entry = await Cache.get(cacheName);
+		if (entry) {
+			return entry;
+		} else {
+			const url = generateURL({ name: packageName, version: packageVersion });
+			const {
+				data: {
+					dependencies = {}
+				}
+			} = await axios.get(url);
 
-		if (Object.keys(dependencies).length) {
-			tree.children = await Promise.all(
-				Object.entries(dependencies)
-					.map(async ([name, version]) => fetchDepsTree({ packageName: name, packageVersion: version }))
-			);
+			if (Object.keys(dependencies).length) {
+				tree.children = await Promise.all(
+					Object.entries(dependencies)
+						.map(async ([name, version]) => fetchDepsTree({ packageName: name, packageVersion: version }))
+				);
+			}
 		}
-		return tree;
 	} catch (e) {
 		if (e.response && e.response.status === 404) {
 			throw new Error('NPM says nope. so nope.');
 		}
-		return tree;
 	}
+	const setReply = await Cache.set(cacheName, tree);
+	console.log({ setReply }, cacheName);
+	return tree;
 };
+
 export {
 	fetchDepsTree
 };
