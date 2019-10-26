14db4c4871270d1f5c7309d7507ed34bd9519366
aviadhahami <aviadhahami@gmail.com>
2019-10-27 00:28:15 +0300

    catch block

--- src/logic/semver/index.js
+++ src/logic/semver/index.js
@@ -1,8 +1,13 @@
 import parseSemver from 'parse-semver';
 
-export const packageInfo = (p, v) => {
-	const { name, version } = parseSemver(`${p}@${v}`);
-	return { name, version };
+export const packageInfo = (p, v = 'latest') => {
+	try {
+		const { name, version } = parseSemver(`${p}@${v}`);
+		return { name, version };
+	} catch (e) {
+		console.log(e.message);
+		return { name: p, version: v };
+	}
 };
 
 export const packageNameForCache = (p, v) => {
