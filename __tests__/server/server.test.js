4b93ee72c2d4e29fe45cd0be00676a5e84995363
aviad.hahami <aviad.hahami@cyiot.net>
2019-10-30 13:07:36 +0200

    basic test for server running

--- __tests__/server/server.test.js
+++ __tests__/server/server.test.js
@@ -2,6 +2,9 @@ import supertest from 'supertest'
 import app from './../../src/'
 
 describe('Server mounting tests', () => {
+	afterAll(()=>{
+		app.close()
+	})
 	it('should mount the server and respond w/ 200 for healthcheck', async() => {
 		const res = await supertest(app)
 			.get('/healthcheck')
