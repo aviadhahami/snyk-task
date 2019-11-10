d82d9e2beb975a6d49bc507953d63b149baf034f
aviad.hahami <aviad.hahami@cyiot.net>
2019-11-10 12:21:40 +0200

    WIP

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import { API } from './../config';
 import { packageInfo } from '../logic/semver';
 
-const LATEST = 'latest';
-const generateURL = ({ name, version = LATEST, fetchPackageInfo = false } = {}) => {
+const generateURL = ({ name, version, fetchPackageInfo = false } = {}) => {
 	if (!name) { throw new Error('No name supplied'); }
 
 	const buffer = [];
@@ -10,12 +9,8 @@ const generateURL = ({ name, version = LATEST, fetchPackageInfo = false } = {}) 
 	buffer.push(name);
 	if (!fetchPackageInfo) {
 		if (!name.includes('@')) {
-			if (version !== LATEST) {
-				const info = packageInfo(name, version);
-				buffer.push((info.version));
-			} else {
-				buffer.push(version);
-			}
+			const info = packageInfo(name, version);
+			buffer.push((info.version));
 		}
 	}
 	return buffer.join('/');
--- src/logic/semver/index.js
+++ src/logic/semver/index.js
@@ -1,5 +1,7 @@
 import parseSemver from 'parse-semver';
 import semver from 'semver';
+import axios from 'axios';
+import { generateURL } from '../../api';
 
 // lodash@2.* ==> lodash@2.5.6 (latest of 2)
 // ^ --> Latest minor
@@ -7,12 +9,17 @@ import semver from 'semver';
 // lodash @ *
 // lodash @ 2.1.x || *
 
-
 // TODO: Cache resolved
 
 export const packageInfo = async (p, v) => {
-
-
+	const url = generateURL({ name: p, fetchPackageInfo: true });
+	const {
+		data: {
+			versions
+		}
+	} = await axios.get(url);
+	const latest = semver.maxSatisfying(Object.keys(versions));
+	console.log(latest);
 };
 
 export const packageNameForCache = (p, v) => {
